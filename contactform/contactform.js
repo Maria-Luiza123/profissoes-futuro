f136f6f44a830be81cf4dd5396ac9943c9c13b89
vtinlopes <168240603+vtinlopes@users.noreply.github.com>
2024-10-16 09:31:09 -0300

    Update contactform.js

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -3,30 +3,30 @@ jQuery(document).ready(function($) {
 
   //Contact
   function valida() { 
-			var nome = contato.nome.value;
-			var email = contato.email.value;
-			var nota = contato.nota.value;
+	var nome = contato.nome.value;
+	var email = contato.email.value;
+	var nota = contato.nota.value;
 		
-			if (nome == "") {
-			   alert('Por favor, Preencha o campo com seu nome!');
-			   contato.nome.focus();
-			return false;
-			}
+	if (nome == "") {
+		alert('Por favor, Preencha o campo com seu nome!');
+		contato.nome.focus();
+		return false;
+		}
 
-			if (email == "") {
-			   alert('Preencha o campo com seu email');
-			   contato.email.focus();
-			return false;
-			}
+	if (email == "") {
+	  	alert('Preencha o campo com seu email');
+		contato.email.focus();
+		return false;
+		}
 
-			if (nota == "") {
-				alert('Preencha o campo com a sua nota!');
-			   contato.nota.focus();
-			return false;
-			}
+	if (nota == "") {
+		alert('Preencha o campo com a sua nota!');
+		contato.nota.focus();
+		return false;
+		}
 			
-			alert("Dados enviados com sucesso!");
-			contato.reset(); 
+	alert("Dados enviados com sucesso!");
+	contato.reset(); 
 
 		}
   });
